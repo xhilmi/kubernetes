bf1a02693dbaa023c8f4a2b212e767675dff9395
xhilmi <hilmiafifi12@gmail.com>
2023-04-24 12:23:28 +0700

    ⚒️ k8s install tools

--- tools/k8s-install-noprompt-master.sh
+++ tools/k8s-install-noprompt-master.sh
@@ -121,7 +121,6 @@ sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd
 sudo systemctl daemon-reload
 sudo systemctl enable cri-docker.service
 sudo systemctl enable --now cri-docker.socket
-sudo systemctl status cri-docker.socket
 lsmod | grep br_netfilter
 sudo systemctl enable kubelet
 sudo ufw disable
