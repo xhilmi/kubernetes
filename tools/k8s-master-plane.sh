92d1efbbab6926327627a1ed5d63ad4508ddda63
X Hilmi <38133643+xhilmi@users.noreply.github.com>
2024-01-14 19:16:14 +0700

    Update k8s-master-plane.sh

--- tools/k8s-master-plane.sh
+++ tools/k8s-master-plane.sh
@@ -52,3 +52,10 @@ sudo kubeadm init --pod-network-cidr=10.10.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
+
+sed -i 's/cidr: 192\.168\.0\.0\/16/cidr: 10.10.0.0\/16/g' custom-resources.yaml
+kubectl create -f custom-resources.yaml
+
