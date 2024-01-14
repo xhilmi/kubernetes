93684b9a04a2448bf9dcea34169da929b3c46aef
X Hilmi <38133643+xhilmi@users.noreply.github.com>
2024-01-14 19:18:07 +0700

    Create k8s-worker-node.sh

---
+++ tools/k8s-worker-node.sh
@@ -0,0 +1,48 @@
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+sudo hostnamectl set-hostname "worker-node"
+exec bash
+
+sudo tee /etc/hosts <<EOF
+10.128.0.3 master-node
+10.128.0.4 worker-node
+EOF
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl
+
+sudo mkdir /etc/apt/keyrings
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt install -y docker.io
+
+sudo mkdir /etc/containerd
+sudo sh -c "containerd config default > /etc/containerd/config.toml" 
+sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+
+sudo systemctl restart containerd.service
+sudo systemctl restart kubelet.service
+sudo systemctl enable kubelet.service
+
