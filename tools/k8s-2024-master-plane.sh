9d6013257f504fdbea1ea6e5d020b7c72375eec7
X Hilmi <38133643+xhilmi@users.noreply.github.com>
2024-01-14 18:08:11 +0700

    Update k8s-2024-master-plane.sh

--- tools/k8s-2024-master-plane.sh
+++ tools/k8s-2024-master-plane.sh
@@ -71,7 +71,7 @@ ${BOLD}"
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/kubernetes.gpg] http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list
 sudo apt update
-sudo apt install kubeadm kubelet kubectl
+sudo apt install kubeadm kubelet kubectl -y
 sudo apt-mark hold kubeadm kubelet kubectl
 
 
